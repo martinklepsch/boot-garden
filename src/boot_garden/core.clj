84d81ea824a4c4665219522ce74d5bba939482c3
Martin Klepsch <martinklepsch@googlemail.com>
2014-11-10 23:55:33 +0100

    use pod for namespace tracking so ns-tracker is only dependency when used

--- src/boot_garden/core.clj
+++ src/boot_garden/core.clj
@@ -1,7 +1,6 @@
 (ns boot-garden.core
   {:boot/export-tasks true}
   (:require [clojure.java.io   :as io]
-            [ns-tracker.core   :as nst]
             [boot.core         :as boot :refer [deftask]]
             [boot.pod          :as pod]
             [boot.file         :as file]
@@ -9,6 +8,9 @@
 
 (def initial
   (atom true))
+
+(defn ns-tracker-pod []
+  (pod/make-pod (assoc-in (boot/get-env) [:dependencies] '[[ns-tracker "0.2.2"]])))
 
 (deftask garden
   "compile garden"
@@ -23,16 +25,20 @@
         ns-sym      (symbol (namespace css-var))
         tgt-dir     (boot/mktgtdir!)
         out         (io/file tgt-dir output-path)
-        changed-ns  (nst/ns-tracker (seq (boot/get-env :src-paths)))]
+        ; FIXME use this in pod/eval-in below instead
+        ; src-paths   (seq (boot/get-env :src-paths))
+        ns-pod      (ns-tracker-pod)
+        _           (pod/require-in-pod ns-pod "ns_tracker.core")
+        _           (pod/eval-in ns-pod (def cns (ns-tracker.core/ns-tracker (seq ~(boot/get-env :src-paths)))))]
     (boot/with-pre-wrap
-      (when (or @initial (some #{ns-sym} (changed-ns)))
-        (let [w   (pod/make-pod (boot/get-env))]
+      (when (or @initial (some #{ns-sym} (pod/eval-in ns-pod (cns))))
+        (let [c-pod   (pod/make-pod (boot/get-env))]
           (if @initial (reset! initial false))
           (util/info "Compiling %s...\n" (.getName out))
           (io/make-parents out)
-          (pod/require-in-pod w "garden.core")
-          (pod/require-in-pod w (str ns-sym))
-          (pod/eval-in w (garden.core/css {:output-to ~(.getPath out)
-                                           :pretty-print ~pretty-print
-                                           :vendors ~vendors
-                                           :auto-prefix ~(set auto-prefix)} ~css-var)))))))
+          (pod/require-in-pod c-pod "garden.core")
+          (pod/require-in-pod c-pod (str ns-sym))
+          (pod/eval-in c-pod (garden.core/css {:output-to ~(.getPath out)
+                                               :pretty-print ~pretty-print
+                                               :vendors ~vendors
+                                               :auto-prefix ~(set auto-prefix)} ~css-var)))))))
